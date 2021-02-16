c0a7c32668a929020c0e991b551c07720478af23
tecsify <79121642+tecsify@users.noreply.github.com>
2021-02-15 18:54:50 -0600

    Create HolaMundo.cs

---
+++ HolaMundo.cs
@@ -0,0 +1,15 @@
+//Tecsify ¡Tecnología que empodera!
+using System; 
+	
+namespace HolaMundo{
+public class Programa
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hola Mundo!");
+	}
+}
+}
+/*
+www.Tecsify.com/blog
+*/
